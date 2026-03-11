b622da92f20a08fd1a02183f2193a7674cb92881
roblancobetes <rodrigoblancobetes@gmail.com>
2026-03-11 14:55:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
   res.send("Hola mundo desde Azure 🚀")
 })
 
-app.listen(PORT, ()=>{
-  console.log("Server running")
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
