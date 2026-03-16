c0a499e07d93a86d268dd2d0fffccd5698050a1c
Rodrigo Blanco (Alfa Formación) <rblanco.docente@alfa-formacion.com>
2026-03-16 09:32:03 +0100

    cambios

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-  res.send("Hola mundo desde Azure 🚀")
+  const podName = process.env.POD_NAME || 'unknown';
+  const podNamespace = process.env.POD_NAMESPACE || 'unknown';
+  res.send(`Hola mundo desde Azure 🚀 - Pod: ${podName} in namespace: ${podNamespace}`)
 })
 
 app.listen(PORT, () => {
